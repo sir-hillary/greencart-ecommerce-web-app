3d33472c028b9f4a4dacd1551ec365ddb78258fe
Hillary Omondi <h.o.omondi15@gmail.com>
2025-11-17 12:55:27 +0300

    updating the backend allowed origins for cors

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ await connectCloudinary();
 
 // allow multiple origins
 const allowedOrigins = [
-    'http://localhost:5173'
+    'https://greencartecommerce.vercel.app'
 ]
 
 app.post('/stripe', express.raw({type: "application/json"}), stripeWebhooks)
